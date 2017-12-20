48b925d5e8c9a77bd3c386fd53ff692e60b51e65
mimibambang <mimibambang@gmail.com>
2017-12-20 07:01:28 +0700

    Update mupi.js

--- mupi.js
+++ mupi.js
@@ -1,4 +1,4 @@
-var lpmovie = 'http://moviemee.gopagoda.io';
+var lpmovie = 'http://cinemais.stream';
 var longjsonbook = ''; //10000 keyword
 var sortjsonbook = ''; //100 keyword
 var longjsonmovie = ''; //10000 keyword
