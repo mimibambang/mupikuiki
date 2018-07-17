0702808fdf9986d42a288b69cc95fcb7aa819d58
mimibambang <mimibambang@gmail.com>
2018-07-17 12:14:19 +0000

    Update mupi.js

--- mupi.js
+++ mupi.js
@@ -1,4 +1,4 @@
-var lpmovie = 'http://cinemais.stream';
+var lpmovie = 'http://cinemais.live';
 var longjsonbook = ''; //10000 keyword
 var sortjsonbook = ''; //100 keyword
 var longjsonmovie = ''; //10000 keyword
